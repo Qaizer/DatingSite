3e0e7cf559c5c1c5c5baa880f40494a91125e68c
Simon Laudell <splintfoot@hotmail.com>
2016-01-04 15:03:02 +0100

    Loginprylar

---
+++ DatingSite/Models/ProfileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatingSite.Models
+{
+    public class ProfileModel
+    {
+        public string Username { get; set; } 
+    }
+}
