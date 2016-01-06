9031141129f957fcaa1c8dd9e8e1d21c0635a1a4
Simon Laudell <splintfoot@hotmail.com>
2016-01-06 13:13:43 +0100

    Börjat på Message.

---
+++ DataAccessLayer/MessageRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class MessageRepository
+    {
+        public void AddMessage(int sender, int reciever, string message, DateTime date)
+        {
+            using (var context = new OnlineDatingDBEntities())
+            {
+                var messageToAdd = new Message
+                {
+                    Sender = sender,
+                    Reciever = reciever,
+                    Message1 = message,
+                    Date = date
+                };
+            }
+        }
+    }
+}
---
+++ DatingSite/Controllers/ApiControllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccessLayer;
+using DatingSite.Models;
+
+namespace DatingSite.Controllers.ApiControllers
+{
+    public class MessageController : ApiController
+    {
+        private UserRepository _userRepository;
+        private MessageRepository _messageRepository;
+
+        public MessageController()
+        {
+            _userRepository = new UserRepository();
+            _messageRepository = new MessageRepository();
+        }
+        public MessageModel SendMessage(string sender, string reciever, string message)
+        {
+            var messageToAdd = new MessageModel(
+                
+                );
+        }
+    }
+}
